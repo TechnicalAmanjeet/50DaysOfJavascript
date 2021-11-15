d907dbfb2a3b7c2ec03e754c7eff03f0e178781a
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-11-15 14:43:15 +0530

    added mathod 2 for day 2

--- Day_2/index.js
+++ Day_2/index.js
@@ -3,9 +3,15 @@ const str = "Javascript is awesome";
 function reverseAString(str){
     // Mathod 1 : itrative approach.
     let revString = "";
-    for(let i=str.length-1;i>=0;--i){
-        revString+=str[i];
-    }
+    // for(let i=str.length-1;i>=0;--i){
+    //     revString+=str[i];
+    // }
+
+    // Mathod 2 : By using Inbuilt function
+    let arr = str.split("");
+    arr.reverse();
+    // console.log(arr);
+    revString = arr.join("");
 
     return revString;
 }
