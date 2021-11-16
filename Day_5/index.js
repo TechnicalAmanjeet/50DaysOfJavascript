9e4c31e9fa0b2930fff367bf1ed831ee5aaf8bd5
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-11-16 11:47:33 +0530

    done with 5th day

---
+++ Day_5/index.js
@@ -0,0 +1,20 @@
+const str = 'f(X) !== G(X) !== F(X)';
+
+// Write a function which accepts a string argument and returns the count of characters between the first and last character 'X'
+
+function getTheGapX(str){
+    var firstIndexofX = str.indexOf("X");
+
+    if(firstIndexofX==-1) return -1;
+
+    var strToArr = str.split("");
+    strToArr.reverse();
+
+    var reversedStr = strToArr.join("");
+
+    var lastIndexOfX = str.length - reversedStr.indexOf("X")-1;
+
+    return lastIndexOfX - firstIndexofX ;
+}
+
+console.log(`Gap between the X's : ${getTheGapX(str)}`);
