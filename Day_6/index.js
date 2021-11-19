01c93e6c51051c811bb01b78247762ff038ed9a5
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-11-19 21:39:02 +0530

    done with 6th day

---
+++ Day_6/index.js
@@ -0,0 +1,19 @@
+const str = 'JavaScript is simple but not easy to master';
+
+const wordLimit = 3;
+// return char till word Limit.
+function truncateWithWordLimit(str,wordLimit){
+    // Mathod 1
+
+    // let arr = str.split(" ");
+    // console.log(arr);
+    // str = arr.slice(0,wordLimit);
+    // console.log(str);
+    // return str.join(" ");
+
+    // Mathod 2 ( By using Chainging )
+    str = str.split(" ").slice(0,wordLimit).join(" ");
+    return str;
+}
+
+console.log(`truncated String : ${truncateWithWordLimit(str,wordLimit)}`);
